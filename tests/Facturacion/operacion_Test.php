6e8762dee9924e6f4b91bb95966cbaa3662c9921
Fernando Ibarra <luisfernandoibarrao@gmail.com>
2024-06-11 13:19:49 -0400

    Cambio_CI

--- tests/Facturacion/operacion_Test.php
+++ tests/Facturacion/operacion_Test.php
@@ -3,13 +3,6 @@ declare (strict_types=1);
 use PHPUnit\Framework\TestCase;
 include "./src/operacion.php";
 class operacion_Test extends TestCase{
-  //  protected function setUp(): void
-    //{
-      //  echo 'Antes de la prueba setup' ;
-    //}
-    //protected function tearDown():void{
-      //  echo 'Despues de la prueba setup' ;  
-  
     public function testFactorial():void{
         $objOpr= new operacion();
         $nFct= $objOpr->factorial(50);
